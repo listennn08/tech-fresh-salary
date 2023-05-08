d90cd62c5a4062e4abfee7c4cdd149334311e791
Matt <listennn08@outlook.com>
2023-05-09 00:16:21 +0800

    feat: add company & interview year filter

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@ import csv from 'csvtojson'
 interface SalaryData {
   [k: string]: string | undefined
   時間戳記?: string
-  公司名稱: string
+  公司: string
   面試年份: string
   職務: string
   '月薪(K)': string
@@ -20,6 +20,8 @@ const company = reactive<string[]>([])
 const industry = reactive<string[]>([])
 const filterIndustry = ref('')
 const filterSalary = ref('')
+const filterCompany = ref('')
+const filterYear = ref('')
 const salaryOptions = reactive([
   '30 以下',
   '31 ~ 40',
@@ -30,6 +32,7 @@ const salaryOptions = reactive([
   '121 ~ 140',
   '140 以上',
 ])
+const yearOptions = reactive<string[]>([])
 const sortBy = reactive({
   name: '',
   by: '',
@@ -38,19 +41,27 @@ const tableData = reactive<SalaryData[]>([])
 try {
   const jsonObj = (await csv().fromString(data.value as string)) as SalaryData[]
   jsonObj.forEach((el) => {
-    if (/\(0\.?\)/.test(el.公司名稱) || el.公司名稱 === '' || !/\d/.test(el['月薪(K)'])) return
-    if (company.indexOf(el.公司名稱) === -1) {
-      company.push(el.公司名稱)
+    if (el.公司 === '' || !/\d/.test(el['月薪(K)'])) return
+    if (company.indexOf(el.公司) === -1 && !el.公司.includes('(0)')) {
+      company.push(el.公司)
     }
     if (industry.indexOf(el.產業類別) === -1 && el.產業類別) {
       industry.push(el.產業類別)
     }
-    el['月薪(K)'] = el['月薪(K)'].replace(/[.>]/g, '').replace(/x/i, Math.floor(Math.random() * 9).toString())
+    el['月薪(K)'] = el['月薪(K)'].replace(/[>]/g, '').replace(/x/i, Math.floor(Math.random() * 9).toString())
+    el['月薪(K)'] = (Number(el['月薪(K)']) < 10 ? Number(el['月薪(K)']) * 10 : el['月薪(K)']).toString()
+    el.面試年份 = el.面試年份.split('/')[0]
+    if (el.面試年份.length === 3) el.面試年份 = (Number(el.面試年份) + 1911).toString()
+
+    if (yearOptions.indexOf(el.面試年份) === -1) {
+      yearOptions.push(el.面試年份)
+    }
     el.recommend = el['面試分享or 推薦事項']
     delete el['面試分享or 推薦事項']
     delete el['時間戳記']
     tableData.push(el)
   })
+  yearOptions.sort((a, b) => Number(b) - Number(a))
 } catch (e) {
   console.log('oops', e)
 }
@@ -81,6 +92,8 @@ const displayData = computed(() => {
           return true
       }
     })
+    .filter((el) => !filterCompany.value || el.公司 === filterCompany.value)
+    .filter((el) => !filterYear.value || el.面試年份 === filterYear.value)
 })
 const reset = () => {
   filterSalary.value = ''
@@ -93,13 +106,20 @@ const reset = () => {
       科技業 剛畢業新鮮人起薪 分享
       <a class="text-base text-primary underline" href="https://www.dcard.tw/f/tech_job/p/241943049" target="_blank">Dcard</a>
     </h1>
-    <div mb="2" flex gap="5" items="center">
-      <div w="lg:1/6" relative mr="2">
+    <div mb="2" flex="~ wrap" gap="5" items="center">
+      <div w="[calc(50%-1.25rem)] lg:1/6" relative>
         <ASelect v-model="filterIndustry" :options="industry" placeholder="產業類別" />
       </div>
-      <div w="lg:1/6" relative>
+      <div w="[calc(50%-1.25rem)] lg:1/6" relative>
         <ASelect v-model="filterSalary" :options="salaryOptions" placeholder="薪資範圍" />
       </div>
+      <div w="[calc(50%-1.25rem)] lg:1/6" relative>
+        <ASelect v-model="filterCompany" :options="company" placeholder="公司" />
+      </div>
+      <div w="[calc(50%-1.25rem)] lg:1/6" relative>
+        <ASelect v-model="filterYear" :options="yearOptions" placeholder="面試年份" />
+      </div>
+
       <ABtn @click="reset">重置</ABtn>
     </div>
     <ADataTable :rows="displayData" search multiSort :page-size="30" no-data-text="沒有資料">
@@ -116,3 +136,9 @@ const reset = () => {
     <a class="text-primary underline" href="https://docs.google.com/spreadsheets/d/1hjvg6Rcy2aI90jT11yuI3MYD6DVow0CuJLuqtz4-IeU/edit" target="_blank">畢業薪資分享 (回覆)</a>
   </footer>
 </template>
+<style lang="postcss">
+.a-list.grid.a-select-options-list {
+  max-height: 200px;
+  overflow-y: scroll;
+}
+</style>
